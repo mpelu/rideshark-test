11675bdc21524c14295b95ec509f71dfa2c070d8
mpelu <pelu0002@algonquinlive.com>
2021-10-24 17:10:58 -0400

    filters entries by date, still no persistence

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let monthNav = 0;
+let clickedDay = null; //TODO initialize to current date
+let entries = localStorage.getItem('entries') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
 
@@ -17,14 +19,14 @@ function loadCalendar(){
 	const lastDayIndex = new Date(year, month + 1, 0).getDay(); 
 	const lastDayOfMonth = new Date(year, month + 1, 0).getDate(); 
 
-	// previous month's last date
+	// previous month's end date
 	const lastDayOfPrevious = new Date(year, month, 0).getDate(); 
 
 	// room left in final week of current month for next month's first week
 	const finalWeek = 6 - lastDayIndex; 
 
 
-	// populating calendar starts here
+	/********** populating calendar starts here **********/
 	calendar.innerHTML = '';
 
 	// last week of the previous month
@@ -34,7 +36,7 @@ function loadCalendar(){
 
 		daySquare.innerText = lastDayOfPrevious - i + 1; 
 
-		daySquare.addEventListener('click', () => console.log(lastDayOfPrevious - i + 1)); 
+		daySquare.addEventListener('click', () => displaySummary(lastDayOfPrevious - i + 1)); 
 
 		calendar.appendChild(daySquare);	
 	}
@@ -45,7 +47,7 @@ function loadCalendar(){
 		daySquare.classList.add('day');
 
 		daySquare.innerText = j;
-		daySquare.addEventListener('click', () => console.log(j));
+		daySquare.addEventListener('click', () => displaySummary(j));
 
 		calendar.appendChild(daySquare);
 	}
@@ -56,11 +58,10 @@ function loadCalendar(){
 		daySquare.classList.add('padding');
 
 		daySquare.innerText = k; 
-		daySquare.addEventListener('click', () => console.log(k));
+		daySquare.addEventListener('click', () => displaySummary(k));
 
 		calendar.appendChild(daySquare);
 	}
-
 }
 
 document.getElementById('nextButton').addEventListener('click', () => {
@@ -77,58 +78,62 @@ document.getElementById('backButton').addEventListener('click', () => {
 loadCalendar();
 
 
+function displaySummary(date){
+	clickedDay = date;
+	const filteredEntries = entryArr.filter(entry => entry.date == date);
 
+	if(filteredEntries.length > 0){
+		document.getElementById("trip-chips").innerHTML = `<div>${filteredEntries}</div>`
+	}else{
+		document.getElementById("trip-chips").innerHTML = `<div>No entries for this date</div>`;
+	}
+
+	
+}
 const entryArr = [];
-
 class Entry{
 	constructor(date, label, type){
 		this.date = date;
 		this.label = label;
 		this.type = type;
 		
-		 entryArr.push(this);
+		entryArr.push(this);
 	}
 
+	saveEntry(){
+		entries.push({
+			date: this.date,
+			label: this.label,
+			type: this.type,
+		})
+		localStorage.setItem('entries', JSON.stringify(entries));
+	}
 	toString(){
 		return JSON.stringify(this);
 	}
-	
 }
-
-// const entryStorage = window.localStorage;
-
-const entry0 = new Entry("March", "did something", "drove carpool");
-const entry1 = new Entry("April", "things", "carpool things");
-const entry2 = new Entry("March", "did something", "some stuff");
-
-let filteredEntries = entryArr.filter(entry => entry.date === "March");
-// document.querySelector(".trip-chips").innerHTML = `<div>${filteredEntries}</div>`
-document.getElementById("trip-chips").innerHTML = `<div>${entryArr}</div>`
-// document.querySelector(".trip-chips").innerHTML = `<div>${entryArr}</div>`
-
-console.log(entryArr);
+const entry0 = new Entry(4, "did something", "drove carpool");
+const entry1 = new Entry(7, "things", "carpool things");
+const entry2 = new Entry(9, "did something", "some stuff");
 
 function userSubmit() {
-	userEntry = new Entry("June", document.getElementById("eLabel").value, document.getElementById("eType").value);
+	userEntry = new Entry(10, document.getElementById("eLabel").value, document.getElementById("eType").value);
 
 	// TODO function to load filtered entries instead of calling document.etc each time
-	document.getElementById("trip-chips").innerHTML = `<div>${entryArr}</div>`
+	// document.getElementById("trip-chips").innerHTML = `<div>${entryArr}</div>`
+	// entries.push({
+	// 	date: 10,
+	// 	label: document.getElementById("eLabel").value,
+	// 	type: document.getElementById("eType").value,
+	// });
+	// localStorage.setItem('entries', JSON.stringify(events));
 
-	// TODO visibility = hidden
+	// TODO: input text disappear
+	// TODO: visibility = false
 	return false;
 };
 
 
-
-
-
-let clicked = null;
-// let entries = localStorage.getItem('entries') ? JSON.parse(localStorage.getItem('entries')) : [];
-
-function displaySummary(date){
-	clicked = date;
-	// TODO
-}
 
 
 
